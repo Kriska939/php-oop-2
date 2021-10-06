930cc4cb736fa0c0a2a78defda8613f784184a49
Kriska939 <cristinacorrado93@gmail.com>
2021-10-06 15:34:12 +0200

    added structure with modules folder

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php 
 
-/* provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+/* 
+ESERCIZIO:
+
+Provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
 Strutturare le classi gestendo ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
 Provate a far interagire tra di loro gli oggetti 
 */
 
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio OOP 2</title>
+</head>
+<body>
+    <!-- CONTENUTO CHE VOGLIO EVENTUALMENTE STAMPARE -->
+</body>
+</html>
